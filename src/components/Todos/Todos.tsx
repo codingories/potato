a656500effc48b40c02566040bfd68d4e5370d4f
Ories Zhu <tellt@126.com>
2020-02-15 18:19:37 +0800

    完成todoItem

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,15 +1,59 @@
 import * as React from 'react';
 // import {Input} from 'antd'
 import TodoInput from 'components/Todos/TodoInput'
+import TodoItem from 'components/Todos/TodoItem'
 import axios from 'config/axios'
 import './Todos.scss'
 
-class Todos extends React.Component {
+interface ITodosState {
+  todos: any[];
+}
+
+class Todos extends React.Component<any, ITodosState> {
+  constructor(props:any){
+    super(props)
+    this.state = {
+      todos: []
+    }
+  }
 
   addTodo = async (params:any)=>{
+    const {todos} = this.state
     try {
       const response = await axios.post('todos',  params)
+      this.setState({todos:[response.data.resource,...todos]})
       console.log(response.data)
+    }catch (e) {
+      throw new Error(e)
+    }
+  }
+
+  componentDidMount(){
+    this.getTodos()
+  }
+
+  getTodos = async () => {
+    try{
+      const response = await axios.get('todos')
+      this.setState({todos:response.data.resources})
+      console.log(response.data);
+    }catch (e) {
+      throw new Error(e)
+    }
+  }
+
+  updateTodo = async (id:number,params:any) => {
+    const {todos} = this.state
+    try {
+      const response = await axios.put( `todos/${id}`, params )
+      const newTodos = todos.map(t=>{
+        if(id === t.id){
+          return response.data.resource // 这里就直接把后台获取的状态反给id
+        } else {
+          return t
+        }
+      })
+      this.setState({todos: newTodos})
     }catch (e) {
       throw new Error(e)
     }
@@ -20,6 +64,13 @@ class Todos extends React.Component {
       <div className="Todos" id="Todos">
         {/*params是里面要把内容传出来*/}
         <TodoInput addTodo={(params:any)=>this.addTodo(params)}/>
+        <main>
+          {
+            this.state.todos.map(t=><TodoItem key={t.id} {...t}
+              update={this.updateTodo}
+            />)
+          }
+        </main>
       </div>
     );
   }
