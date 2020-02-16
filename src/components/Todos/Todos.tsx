2b745b1ffdfa49c46d6e00c9322bee7835c2bfa3
Ories Zhu <tellt@126.com>
2020-02-16 15:18:46 +0800

    完成 优化 todos

--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from 'react'
 // import {Input} from 'antd'
 import TodoInput from 'components/Todos/TodoInput'
 import TodoItem from 'components/Todos/TodoItem'
@@ -10,83 +10,105 @@ interface ITodosState {
 }
 
 class Todos extends React.Component<any, ITodosState> {
-  constructor(props:any){
+  constructor(props: any) {
     super(props)
     this.state = {
       todos: []
     }
   }
 
-  addTodo = async (params:any)=>{
+  get unDeletedTodos() {
+    return this.state.todos.filter(t => !t.deleted)
+  }
+
+  get unCompletedTodos() {
+    return this.unDeletedTodos.filter(t => !t.completed)
+  }
+
+  get CompletedTodos() {
+    return this.unDeletedTodos.filter(t => t.completed)
+  }
+
+
+  addTodo = async (params: any) => {
     const {todos} = this.state
     try {
-      const response = await axios.post('todos',  params)
-      this.setState({todos:[response.data.resource,...todos]})
+      const response = await axios.post('todos', params)
+      this.setState({todos: [response.data.resource, ...todos]})
       console.log(response.data)
-    }catch (e) {
+    } catch (e) {
       throw new Error(e)
     }
   }
 
-  componentDidMount(){
+
+  componentDidMount() {
     this.getTodos()
   }
 
   getTodos = async () => {
-    try{
+    try {
       const response = await axios.get('todos')
-      const todos = response.data.resources.map((t:any)=>Object.assign({},t,{editing:false}))
+      const todos = response.data.resources.map((t: any) => Object.assign({}, t, {editing: false}))
       // 添加editing:false来判断是否是可编辑状态
       this.setState({todos})
-      console.log(response.data);
-    }catch (e) {
+      console.log(response.data)
+    } catch (e) {
       throw new Error(e)
     }
   }
 
-  updateTodo = async (id:number,params:any) => {
+  updateTodo = async (id: number, params: any) => {
     const {todos} = this.state
     try {
-      const response = await axios.put( `todos/${id}`, params )
-      const newTodos = todos.map(t=>{
-        if(id === t.id){
+      const response = await axios.put(`todos/${id}`, params)
+      const newTodos = todos.map(t => {
+        if (id === t.id) {
           return response.data.resource // 这里就直接把后台获取的状态反给id
         } else {
           return t
         }
       })
       this.setState({todos: newTodos})
-    }catch (e) {
+    } catch (e) {
       throw new Error(e)
     }
   }
 
-  toEditing = (id:number) => {
+  toEditing = (id: number) => {
     const {todos} = this.state
-    const newTodos = todos.map(t=>{
-      if(id===t.id){
-        return Object.assign({},t,{editing:true})
+    const newTodos = todos.map(t => {
+      if (id === t.id) {
+        return Object.assign({}, t, {editing: true})
       } else {
-        return Object.assign({},t,{editing:false})
+        return Object.assign({}, t, {editing: false})
       }
     })
-    this.setState({todos:newTodos})
+    this.setState({todos: newTodos})
   }
 
   public render() {
-    return(
+    return (
       <div className="Todos" id="Todos">
         {/*params是里面要把内容传出来*/}
-        <TodoInput addTodo={(params:any)=>this.addTodo(params)}/>
-        <main>
+        <TodoInput addTodo={(params: any) => this.addTodo(params)}/>
+        <div className="todoLists">
           {
-            this.state.todos.map(t=><TodoItem key={t.id} {...t}
-              update={this.updateTodo} toEditing={this.toEditing}
+            this.unCompletedTodos.map(t => <TodoItem key={t.id} {...t}
+            update={this.updateTodo} toEditing={this.toEditing}
             />)
           }
-        </main>
+          {
+            this.CompletedTodos.map(t => <TodoItem
+            key={t.id} {...t}
+            update={this.updateTodo}
+            toEditing={this.toEditing}
+            />)
+          }
+        </div>
       </div>
-    );
+    )
   }
 }
-export default Todos;
+
+export default Todos
